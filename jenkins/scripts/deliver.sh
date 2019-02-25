6509096176fe9e45a10cb5c33aa1f8d0208b2dc8
fly10000 <solarfly10000@gmail.com>
2019-02-25 15:47:13 +0800

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env bash
-su - root <<EOF
-scp target/tm-0.0.1-SNAPSHOT.jar root@10.15.4.48:/opt/jenkins/
-pwd;
-exit;
-EOF
+sudo scp target/tm-0.0.1-SNAPSHOT.jar root@10.15.4.48:/opt/jenkins/

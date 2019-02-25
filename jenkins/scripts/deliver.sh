b01644da506ec97229e6f0614e7d9cc83cd64aae
fly10000 <solarfly10000@gmail.com>
2019-02-25 14:35:30 +0800

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,4 +23,4 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar
+scp target/${NAME}-${VERSION}.jar root@10.15.4.48
